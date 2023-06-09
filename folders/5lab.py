675b482d57c13586f3e8c38887dc45ea9b485eb1
LEVES27 <127956242+LEVES27@users.noreply.github.com>
2023-06-09 17:59:29 +0300

    Update 5lab.py

--- folders/5lab.py
+++ folders/5lab.py
@@ -1,8 +1,12 @@
 from num2word import word
+
+# Виводимо привітання
 print("Hello!")
 
 """Asks for user integer num"""
+# Запитуємо користувача про ціле число
 num = input("Enter your number: ")
 
 """Prints user number as text"""
-print("Your number is: "+ word(num))
+# Виводимо число користувача у текстовому форматі
+print("Your number is: " + word(num))
